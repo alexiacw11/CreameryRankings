b64bd6bada6c458192e46baa24d453ac12e828fa
alexiacw11 <130490875+alexiacw11@users.noreply.github.com>
2024-08-30 23:42:19 -0600

    Update IceCreamParty.R

--- IceCreamParty.R
+++ IceCreamParty.R
@@ -1,9 +1,3 @@
-# Alexia's Notes
-#1. Should we do less ice cream flavors if we 87 students coming? 
-#2. Might need to normalize faculty data...
-#3. Created some extra tables/graphs but can be deleted
-#4. Might want to make histogram different color that is easier to see?
-
 # Get working directory
 getwd()
 
@@ -19,14 +13,14 @@ library(patchwork)
 library(hrbrthemes)
 library(knitr)
 library(gt)
-library(RColorBrewer) #scale_fill_brewer(palette="Dark2"), might want to do different color histograms
+library(RColorBrewer)
 
 # Reading in data
 rankings <- read.csv("Creamery_Rankings.csv")
-rankings <- read.csv("Creamery Rankings - Actual.csv")
 
+# Remove two columns
 rankings <- rankings |> 
-  dplyr::select(-Timestamp)
+  dplyr::select(-Timestamp, -Email)
   
 # View full dataset
 View(rankings)
@@ -35,34 +29,36 @@ View(rankings)
 head(rankings)
 
 # Factorization
-rankings <- rankings |> mutate_at(c('Role', 'Sex'), as.factor)
+rankings <- rankings |> mutate_at(c('Role', 'I.am'), as.factor)
 
 #--------------------------------------------------------------------------------
 # EDA - Exploratory Data Analysis 
 # Getting a feel for how the data looks/is
 
 # Reshape data
-long_rankings <- rankings |>
-  pivot_longer(cols = starts_with("Ice.Cream"),
-               names_to = "Flavor",
-               values_to = "Rank")
+long_rankings <- rankings %>%
+  pivot_longer(
+    cols = starts_with("Blue.Goggles"):starts_with("Sparkle.Sherbet"),
+    names_to = "Flavor",
+    values_to = "Rank"
+  )
 
+# What is the proportion of faculty vs. student?
+ggplot(data=rankings, mapping=aes(x=Role, fill=Role)) + 
+  geom_bar() + ggtitle("Proportion of Faculty vs. Students Participants") + 
+  xlab("Count") + ylab("Particpants")
 
 # Boxplot
-ggplot(long_rankings, aes(Role, Rank, fill=Flavor)) +
+ggplot(long_rankings, aes(Flavor, Rank, fill=Flavor)) +
   geom_boxplot(width=0.7) +
-  ggtitle("Boxplot of Student vs. Faculty Rankings Filled by Flavor")
-
-# Print Numerical Summary for rank...except it is obvious
-favstats(~Rank, data=long_rankings)
-
-#Real n, above is just different because using long version of data
-favstats(~Rank, data=long_rankings)$n / 7
+  ggtitle("Boxplot of Rankings by Flavor") +
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 
 # Creating a tibble of means per flavor to show in a table 
-means.df <- tibble(One = mean(rankings$Ice.Cream..1), Two = mean(rankings$Ice.Cream..2), 
-Three = mean(rankings$Ice.Cream..3), Four = mean(rankings$Ice.Cream..4),Five = mean(rankings$Ice.Cream..5), 
-Six = mean(rankings$Ice.Cream..6), Seven = mean(rankings$Ice.Cream..7))
+means.df <- long_rankings %>%
+  group_by(Flavor) %>%
+  summarize(Mean=round(mean(Rank),2)) %>%
+  arrange(Mean)
 
 gt(means.df) |> 
   tab_header(
@@ -70,17 +66,19 @@ gt(means.df) |>
     subtitle = "Faculty and Student Scores Included"
   )
 
+# Creating a tibble of means per flavor considering Role
+means_role.df <- long_rankings %>%
+  group_by(Flavor, Role) %>%
+  summarize(Mean=round(mean(Rank),2)) %>%
+  arrange(Mean)
 
-# What is the proportion of faculty vs student?
-ggplot(data=rankings, mapping=aes(x=Role, fill=Role)) + 
-  geom_bar() + ggtitle("Proportion of Faculty vs. Students Participants") + 
-  xlab("Count") + ylab("Particpants")
+gt(means_role.df) |> 
+  tab_header(
+    title = md("**Average Ranking for Each Flavor of Ice Cream**"),
+    subtitle = "Seperated by Faculty and Student Scores"
+  )
 
 
-# What about for sex? 
-ggplot(data=rankings, mapping=aes(x=Sex, fill=Sex)) + 
-  geom_bar() + ggtitle("Proportion of Participants by Sex") + 
-  xlab("Sex") + ylab("Sex")
 
 # Create heatmap, can be by name or we could do it by roll of faculty vs. students
 ggplot(long_rankings, aes(x = Flavor, y = Name, fill = Rank)) +
@@ -95,84 +93,37 @@ ggplot(long_rankings, aes(x = Flavor, y = Name, fill = Rank)) +
 #--------------------------------------------------------------------------------
 # T-test between student and faculty average ranking of an ice cream flavor
 
-# Question: Is there a difference between the student and faculty 
-#average ranking of an ice cream flavor?
-
-
-# Null Hypothesis: There is no difference between the average faculty and student ranking of
-# the selected ice cream flavor. 
-# Alternative Hypothesis: There is a difference between the average faculty and student ranking
-# of the selected ice cream flavor. 
-
+# Question: Is there a difference between all students and all faculty 
+#average ranking of Strawberry Sundae Crunch?
 
 # Explanatory variable (x): Role (either student or faculty)
 # Quantitative variable (y): Rank
 
-
-# In order to do a t-test, there are conditions that must be checked
-# 1. Distribution is normal
-# 2. Similar variance in each group
-
-
-# Separating student and faculty rankings again
+# Separating student and faculty rankings
 student_rankings <- rankings |> 
   filter(Role == "Student")
 
 faculty_rankings <- rankings |> 
   filter(Role == "Faculty")
 
-
-# Density Plot for Student Rankings
-ggplot() + 
-  geom_density(data = student_rankings, mapping=aes(x=Ice.Cream..1, color="Ice Cream #1")) + 
-  geom_density(data = student_rankings, mapping=aes(x=Ice.Cream..2, color="Ice Cream #2")) + 
-  geom_density(data = student_rankings, mapping=aes(x=Ice.Cream..3, color="Ice Cream #3")) + 
-  geom_density(data = student_rankings, mapping=aes(x=Ice.Cream..4, color="Ice Cream #4")) + 
-  geom_density(data = student_rankings, mapping=aes(x=Ice.Cream..5, color="Ice Cream #5")) + 
-  geom_density(data = student_rankings, mapping=aes(x=Ice.Cream..6, color="Ice Cream #6")) + 
-  geom_density(data = student_rankings, mapping=aes(x=Ice.Cream..7, color="Ice Cream #7")) +
-  ggtitle("Density Plot of Student Rankings by Flavor") + 
-  xlab("Rank") + ylab("Density") + labs(color = "Flavor")
-
-# Density Plot for Faculty Rankings
-ggplot() + 
-  geom_density(data = faculty_rankings, mapping=aes(x=Ice.Cream..1, color="Ice Cream #1")) + 
-  geom_density(data = faculty_rankings, mapping=aes(x=Ice.Cream..2, color="Ice Cream #2")) + 
-  geom_density(data = faculty_rankings, mapping=aes(x=Ice.Cream..3, color="Ice Cream #3")) + 
-  geom_density(data = faculty_rankings, mapping=aes(x=Ice.Cream..4, color="Ice Cream #4")) + 
-  geom_density(data = faculty_rankings, mapping=aes(x=Ice.Cream..5, color="Ice Cream #5")) + 
-  geom_density(data = faculty_rankings, mapping=aes(x=Ice.Cream..6, color="Ice Cream #6")) + 
-  geom_density(data = faculty_rankings, mapping=aes(x=Ice.Cream..7, color="Ice Cream #7")) +
-  ggtitle("Density Plot of Faculty Rankings by Flavor") + 
-  xlab("Rank") + ylab("Density") + labs(color = "Flavor")
- 
-
 # Histograms by flavor
-one <- ggplot(data=rankings, mapping=aes(x=Ice.Cream..1, fill=Role)) + 
-  geom_histogram(position='identity', binwidth=0.5, alpha=0.5) +
-  ggtitle("Ice Cream #1") + xlab("Rank")
-two <- ggplot(data=rankings, mapping=aes(x=Ice.Cream..2, fill=Role)) + 
-  geom_histogram(position='identity', binwidth=0.5, alpha=0.5) +
-  ggtitle("Ice Cream #2") + xlab("Rank")
-three <- ggplot(data=rankings, mapping=aes(x=Ice.Cream..3, fill=Role)) + 
-  geom_histogram(position='identity', binwidth=0.5, alpha=0.5) +
-  ggtitle("Ice Cream #3") + xlab("Rank")
-four <- ggplot(data=rankings, mapping=aes(x=Ice.Cream..4, fill=Role)) + 
-  geom_histogram(position='identity', binwidth=0.5, alpha=0.5) +
-  ggtitle("Ice Cream #4") + xlab("Rank")
-five <- ggplot(data=rankings, mapping=aes(x=Ice.Cream..5, fill=Role)) + 
-  geom_histogram(position='identity', binwidth=0.5, alpha=0.5) +
-  ggtitle("Ice Cream #5") + xlab("Rank")
-six <- ggplot(data=rankings, mapping=aes(x=Ice.Cream..6, fill=Role)) + 
-  geom_histogram(position='identity', binwidth=0.5, alpha=0.5) +
-  ggtitle("Ice Cream #6") + xlab("Rank")
-seven <- ggplot(data=rankings, mapping=aes(x=Ice.Cream..7, fill=Role)) + 
-  geom_histogram(position='identity', binwidth=0.5, alpha=0.5) +
-  ggtitle("Ice Cream #7") + xlab("Rank")
+one <- ggplot(data=rankings, mapping=aes(x=Blue.Goggles)) +
+  geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="red") +
+  ggtitle("Blue Googles") + xlab("Rank")
+two <- ggplot(data=rankings, mapping=aes(x=Cookies...Cream)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="orange") + ggtitle("Cookies and Cream") + xlab("Rank")
+three <- ggplot(data=rankings, mapping=aes(x=Earnestly.Chocolate)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="yellow") + ggtitle("Earnestly Chocolate") + xlab("Rank")
+four <- ggplot(data=rankings, mapping=aes(x=Graham.Canyon)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="darkolivegreen1") + ggtitle("Graham.Canyon") + xlab("Rank")
+five <- ggplot(data=rankings, mapping=aes(x=Lemon.Berry.Cheesecake)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="springgreen4") + ggtitle("Lemon Berry Cheesecake") + xlab("Rank")
+six <- ggplot(data=rankings, mapping=aes(x=Mint.Chocolate.Chip)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="aquamarine3") + ggtitle("Mint Chocolate Chip") + xlab("Rank")
+seven <- ggplot(data=rankings, mapping=aes(x=Peanut.Butter.Trails)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="steelblue2") + ggtitle("Peanut Butter Trails") + xlab("Rank")
+eight <- ggplot(data=rankings, mapping=aes(x=Pralines...Caramel)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="blue") + ggtitle("Pralines and Caramel") + xlab("Rank")
+nine <- ggplot(data=rankings, mapping=aes(x=Raspberry.and.Cream.Cheese)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="mediumpurple") + ggtitle("Raspberry and Cream Cheese") + xlab("Rank")
+ten <- ggplot(data=rankings, mapping=aes(x=Roasted.Almond.Fudge)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill="mediumorchid1") + ggtitle("Roasted Almond Fudge") + xlab("Rank")
+eleven <- ggplot(data=rankings, mapping=aes(x=Strawberry.Sundae.Crunch)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5,  fill="violetred1") + ggtitle("Strawberry Sundae Crunch") + xlab("Rank")
+twelve <- ggplot(data=rankings, mapping=aes(x=Sparkle.Sherbet)) + geom_histogram(position='identity', binwidth=0.5, alpha=0.5, fill ="hotpink") + ggtitle("Sparkle Sherbet") + xlab("Rank")
 
-#This is all the histograms! Is our data normal? Do we need CLT?
-one + two + three + four + five + six + seven
-
+#This is all the histograms! Is our data normal?
+one + two + three + four + five + six + seven + eight + nine + ten + eleven + twelve
 
 # Use long rankings to check standard deviations of both groups 
 long_faculty <- long_rankings |> 
@@ -187,30 +138,27 @@ long_students <- long_rankings |>
 # Max(s1, s2) / min (s1, s2) < 2
 sd(long_faculty$Rank) 
 sd(long_students$Rank) 
-# max()/min() 
-# Is it appropriate to continue with our t-distribution? 
 
 # Great, lets do the t.test now
 # Change x and y based on which flavor you want to test. 
-t.test(x = student_rankings$Ice.Cream..6, y=faculty_rankings$Ice.Cream..6, alternative = "two.sided", mu = 0, paired = FALSE, var.equal = TRUE,
+t.test(x = student_rankings$Strawberry.Sundae.Crunch, y=faculty_rankings$Strawberry.Sundae.Crunch, alternative = "two.sided", mu = 0, paired = FALSE, var.equal = TRUE,
        conf.level = 0.95)
 
-# From the t-test, we draw a conclusion! Was p low or high?
+# From the t-test, we draw a conclusion! The p was low in comparison to .05!
 
+# Given the p-value of 0.0456 and the alpha level of 0.05, we reject 
+# the null hypothesis and conclude that there is a difference in the 
+# average Strawberry Sundae Crunch ranking between faculty and students.
 #--------------------------------------------------------------------------------
 #2. Cluster Students based on ice cream preference
 
 # Might be smart to run this just in case 
 dev.off()
 
-# Get rankings for only the students
-student_rankings <- rankings |> 
-  filter(Role == "Student") |> 
-  group_by(Name) |> 
-  dplyr::select(starts_with("Ice.Cream"))
+set.seed(123)
 
 # Compute distance between students based on rankings
-suppressWarnings(distance_matrix <- dist(student_rankings, method = "euclidean"))
+suppressWarnings(distance_matrix <- dist(as.matrix(rankings), method = "euclidean"))
 
 # Just making sure there is no NAS
 sum(is.na(distance_matrix))
@@ -218,12 +166,24 @@ sum(is.na(distance_matrix))
 # Hierarchical clustering
 clustering <- hclust(distance_matrix, method = "complete")
 
-# Plotting below
-
-# With the student number
-plot(clustering, main = "Hierarchical Clustering of Student Creamery Rankings", xlab = "Students", ylab = "Distance")
-
 # With the student name
-plot(clustering, student_rankings$Name, main = "Hierarchical Clustering of Student Creamery Rankings", xlab = "Students", ylab = "Distance")
+plot(clustering, rankings$Name, main = "Hierarchical Clustering of Student Creamery Rankings", xlab = "Students", ylab = "Distance")
+
+#Cut the dendrogram to create six clusters
+num_clusters <- 10
+cluster_labels <- cutree(clustering, k = num_clusters)
+
+# Add the cluster assignments to your original data
+rankings$Cluster <- cluster_labels
+
+ClusterNumber <- rankings |>
+  dplyr::select(Cluster, Name) |>
+  group_by(Cluster) |>
+  dplyr::summarize(Names = paste(Name, collapse = ", "), .groups = 'drop') |>
+  arrange(Cluster)
+
+ClusterNumber |> 
+  kbl() |> 
+  kable_styling()
 
 #Yay! Time to meet your ice cream buddy :)
